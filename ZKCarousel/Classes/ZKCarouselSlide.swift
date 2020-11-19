a370e2ed9685e7a405c646a63a7adff1361467ee
Alessandro Zoffoli <alessandro.zoffoli@watchguard.com>
2020-11-18 22:39:22 -0300

    Revert to simplify

--- ZKCarousel/Classes/ZKCarouselSlide.swift
+++ ZKCarousel/Classes/ZKCarouselSlide.swift
@@ -9,18 +9,10 @@ public struct ZKCarouselSlide {
     public var image : UIImage?
     public var title : String?
     public var description: String?
-    public var properties: ZKCarouselSlideProperties = ZKCarouselSlideProperties()
     
     public init(image: UIImage?, title: String?, description: String?) {
         self.image = image
         self.title = title
         self.description = description
-    }
-    
-    public init(image: UIImage?, title: String?, description: String?, properties: ZKCarouselSlideProperties) {
-        self.image = image
-        self.title = title
-        self.description = description
-        self.properties = properties
     }
 }
